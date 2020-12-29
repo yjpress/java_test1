6af248c8a50439082a9b0329db7709c475bb0178
yjpress <yjpress@hanmail.net>
2020-12-29 14:44:35 +0900

    자바프로젝트

---
+++ src/main/java/com/estplaza/myapp/AsunEstApplication.java
@@ -0,0 +1,13 @@
+package com.estplaza.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsunEstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsunEstApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dto/Test2.java
@@ -0,0 +1,8 @@
+package dto;
+import lombok.Data;
+@Data
+public class Test2 {
+	private String name;
+	private int price;
+	private int age;
+}
